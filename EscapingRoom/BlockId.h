6106711fad7ebdc20fedc6c809cdefe180bbe64b
DMoNex <68813204+DMoNex@users.noreply.github.com>
2021-11-16 00:44:37 +0900

    BlockID.h Update (enum 0~19)
    enum 0 : AIR ~ 19 : Arrow Trap

--- EscapingRoom/BlockId.h
+++ EscapingRoom/BlockId.h
@@ -1,6 +1,24 @@
 #pragma once
 
 enum class BlockId {
-	AIR,
-	ROOM
+	AIR, 			// 0
+	ROOM,
+	PLAYER,
+	DOOR_CLOSED,
+	DOOR_OPENED,
+	WALL, 			// 5
+	STAIRS,
+	PORTAL,
+	LADDER,
+	PAD,
+	BOX, 			// 10
+	BALLOON,
+	SWITCH,
+	LASER_SENDER,
+	LASER_RECEIVER,
+	PRISM_BLOCK, 		// 15
+	MIRROR_BLOCK,
+	THORNS_TRAP,
+	THORNS_TRAP_FIXED,
+	ARROW_TRAP 		// 19
 };
