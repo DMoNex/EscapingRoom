b34bb716c93e34bb90e5cb0f7824f4697a5bbcb7
DMoNex <68813204+DMoNex@users.noreply.github.com>
2021-11-22 03:32:58 +0900

    21/11/22 Update
    Block Preset (Id : ROOM) Update
    21/11/22

--- EscapingRoom/Block.cpp
+++ EscapingRoom/Block.cpp
@@ -1,4 +1,10 @@
 #include "pch.h"
 #include "Block.h"
 
-Block Block::AIR(BlockId::AIR);
+Block Block::AIR(BlockId::AIR, 0);
+Block Block::LOOP(BlockId::ROOM, 1);
+Block Block::FLOOR(BlockId::ROOM, 0);
+Block Block::LEFT_WALL(BlockId::ROOM, 5); // -> Left Wall이면 반댓면(Right side)활성화 ... 
+Block Block::RIGHT_WALL(BlockId::ROOM, 4);
+Block Block::FORWARD_WALL(BlockId::ROOM, 3);
+Block Block::BACK_WALL(BlockId::ROOM, 2);
