e42222f07f1724fdb274b2273e21ed5888ca52f9
김일영 <humdrum00001010@gmail.com>
2021-11-29 21:06:41 +0900

    pad -> door fixing.

--- EscapingRoom/World.cpp
+++ EscapingRoom/World.cpp
@@ -292,11 +292,11 @@ Vec3 World::getNextPortal(Vec3 loc) {
 }
 
 void World::closeDoor() {
-	setBlock(door, doorLoc1.x, doorLoc1.y, doorLoc1.z);
-	setBlock(door, doorLoc2.x, doorLoc2.y, doorLoc2.z);
+	setBlock(Block(BlockId::DOOR_CLOSED, door.align), doorLoc1.x, doorLoc1.y, doorLoc1.z);
+	setBlock(Block(BlockId::DOOR_CLOSED, door.align), doorLoc2.x, doorLoc2.y, doorLoc2.z);
 }
 
 void World::openDoor() {
-	setBlock(door, doorLoc1.x, doorLoc1.y, doorLoc1.z);
-	setBlock(door, doorLoc2.x, doorLoc2.y, doorLoc2.z);
+	setBlock(Block(BlockId::DOOR_OPENED, door.align), doorLoc1.x, doorLoc1.y, doorLoc1.z);
+	setBlock(Block(BlockId::DOOR_OPENED, door.align), doorLoc2.x, doorLoc2.y, doorLoc2.z);
 }
